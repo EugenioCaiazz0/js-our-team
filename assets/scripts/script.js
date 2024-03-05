909b207d43071fd79c0a9a5cc5af762f0378b600
EugenioCaiazz0 <eugeniodanilenko@icloud.com>
2024-03-05 15:25:09 +0100

    Setup

---
+++ assets/scripts/script.js
@@ -0,0 +1,28 @@
+
+const team = 
+[
+    {
+     ID: 'Wayne Barrett',
+     teamRole: `Founder & CEO`,
+    },
+    {
+        ID: 'Angela Caroll',
+        teamRole: `Chief Editor`,
+       },
+       {
+        ID: 'Walter Gordon',
+        teamRole: `Office Manager`,
+       },
+       {
+        ID: 'Angela Lopez',
+        teamRole: `Social Media Manager`,
+       },
+       {
+        ID: 'Scott Estrada',
+        teamRole: `Developer`,
+       },
+       {
+        ID: 'Barbara Ramos',
+        teamRole: `Graphic Designer`,
+       }
+]
